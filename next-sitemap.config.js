5d7f31799f5070a412cc4b32a26d045f48bfa2bd
Sami-Aml <amallou_sami@hotmail.com>
2024-06-27 21:31:17 +0200

    update to www

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://flexistrive.com',
+    siteUrl: process.env.SITE_URL || 'https://www.flexistrive.com',
     generateRobotsTxt: true,
     sitemapSize: 7000,
     outDir: './public'
